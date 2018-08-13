df50deff1ae8577fff0f23747b61af7e3d275261
leekarho <k-h_lee@hotmail.co.uk>
2018-08-13 15:39:31 +0100

    challenge step4

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,16 @@
+require 'oystercard'
+
 describe Oystercard do
-  
+  it 'sets default value to 0' do
+    expect(subject.balance).to eq 0
+  end
+
+  it 'adds money to the oystercard' do
+    expect(subject).to respond_to(:add_money).with(1).argument
+  end
+
+  it 'adds £50 to oystercard' do
+    expect(subject.add_money(50)).to eq subject.balance
+  end
+
 end
